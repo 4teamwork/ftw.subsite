be0dfff506e6a4a14bdbdcb7259c1c4e82958fb4
Timon Tschanz <timon.tschanz@4teamwork.ch>
2012-11-26 16:34:04 +0100

    add additional_css viewlet.

---
+++ ftw/subsite/viewlets/cssviewlet.py
@@ -0,0 +1,28 @@
+from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.navigation.root import getNavigationRoot
+from ftw.subsite.interfaces import ISubsite
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class CSSViewlet(ViewletBase):
+
+    template = ViewPageTemplateFile('cssviewlet.pt')
+
+    def __init__(self, context, request, view, manager=None):
+        super(CSSViewlet, self).__init__(context, request, view, manager)
+        self.nav_root = None
+
+    def render(self):
+        self.nav_root = self.context.restrictedTraverse(
+            getNavigationRoot(self.context))
+        if ISubsite.providedBy(self.context):
+            return self.template()
+        else:
+            return ''
+
+
+    def get_css(self):
+        """
+        ddd
+        """
+        return self.nav_root.additional_css
