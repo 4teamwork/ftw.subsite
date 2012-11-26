29f87307999c70a584851b3df80836daae235c86
Timon Tschanz <timon.tschanz@4teamwork.ch>
2012-11-26 14:42:18 +0100

    update teaserportlet

--- ftw/subsite/portlets/teaserportlet.py
+++ ftw/subsite/portlets/teaserportlet.py
@@ -8,7 +8,11 @@ from z3c.form import form, button, field
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFPlone import PloneMessageFactory as _
 from plone.formwidget.contenttree import PathSourceBinder
-from plone.app.form.widgets.wysiwygwidget import WYSIWYGWidget
+from plone.formwidget.contenttree import ContentTreeFieldWidget
+from plone.app.z3cform.wysiwyg.widget import WysiwygFieldWidget
+from z3c.form.interfaces import INPUT_MODE
+from plone.namedfile.field import NamedImage
+from Acquisition import aq_inner, aq_parent
 
 
 class ITeaserPortlet(IPortletDataProvider):
@@ -17,37 +21,33 @@ class ITeaserPortlet(IPortletDataProvider):
 
     teasertitle = schema.TextLine(title=_(u'Teaser Titel'),
                                   description=_(u'Geben Sie einen Titel ein'),
-                                  default=u'Title',
                                   required=True)
 
     teaserdesc = schema.Text(title=_(u'Teaser Beschreibung'),
                              description=_(u"Geben Sie einen Beschrieb ein."),
-                             default=_(u'description'),
                              required=True)
 
     internal_target = schema.Choice(title=_(u"Internal Target"),
-                                    description=_(u"Find an internal target for this image to link to"),
-                                    required=False,
-                                    source=PathSourceBinder({})
-                                   )
-    image = schema.Field(title=u'Image field',
+                                    description=_(u"Find an internal target \
+ for this image to link to"),
+                                   required=True,
+                                   source=PathSourceBinder({})
+                                    )
+    image = NamedImage(title=u'Image field',
                         description=u"Add or replace image for the portlet",
-                        required=False)
-
-
+                        required=True)
 
 
 class Assignment(base.Assignment):
     implements(ITeaserPortlet)
 
-
-    def __init__(self, assignment_context_path=None, teasertitle='', teaserdesc='', image=None, internal_target=None):
+    def __init__(self, assignment_context_path=None, teasertitle='',
+                 teaserdesc='', image=None, internal_target=None):
         self.assignment_context_path = assignment_context_path
         self.teasertitle = teasertitle
         self.teaserdesc = teaserdesc
         self.image = image
         self.internal_target = internal_target
-
 
     @property
     def title(self):
@@ -58,10 +58,6 @@ class Assignment(base.Assignment):
 
 
 class Renderer(base.Renderer):
-    def __init__(self, context, request, view, manager, data):
-        self.context = context
-        self.data = data
-        self.request = request
 
     render = ViewPageTemplateFile('teaserportlet.pt')
 
@@ -74,72 +70,104 @@ class Renderer(base.Renderer):
     @property
     @memoize
     def internal_obj(self):
-        state=getMultiAdapter((self.context, self.request),
+        state = getMultiAdapter((self.context, self.request),
                               name="plone_portal_state")
-        portal=state.portal()
+        portal = state.portal()
         object_path = self.data.internal_target
 
         if isinstance(object_path, unicode):
             object_path = object_path.encode('utf8')
 
-        if object_path is None or len(object_path)==0:
+        if object_path is None or len(object_path) == 0:
             return None
         #XXX: we have to cut off the /
-        if object_path[0]=='/':
-                   object_path = object_path[1:]
+        if object_path[0] == '/':
+            object_path = object_path[1:]
         return portal.restrictedTraverse(object_path, default=None)
 
     @property
     @memoize
     def image_tag(self):
         if self.data.image:
-            state=getMultiAdapter((self.context, self.request),
+            state = getMultiAdapter((self.context, self.request),
                                   name="plone_portal_state")
-            portal=state.portal()
+            portal = state.portal()
             assignment_url = \
                     portal.unrestrictedTraverse(
                 self.data.assignment_context_path).absolute_url()
-            width = self.data.image.width
-            height = self.data.image.height
-            return "<img src='%s/%s/@@image' width='%s' height='%s' alt=''/>" % \
+            return "<img src='%s/%s/@@image' alt=''/>" % \
                    (assignment_url,
-                    self.data.__name__,
-                    str(width),
-                    str(height)
+                    self.data.__name__
                     )
         return ''
-
 
 
 class AddForm(form.AddForm):
     fields = field.Fields(ITeaserPortlet)
 
-
-
     label = _(u"Add Teaser Portlet")
     description = _(u"Shows the given infos on the front-page.")
 
+    def __init__(self, context, request):
+        super(AddForm, self).__init__(context, request)
+        self._finishedAdd = None
+        self.status = None
 
     def updateWidgets(self):
-        self.fields['teaserdesc'].widgetFactory = WYSIWYGWidget
+        self.fields['teaserdesc'].widgetFactory[INPUT_MODE] = WysiwygFieldWidget
         self.fields['internal_target'].widgetFactory = ContentTreeFieldWidget
-        super(EditFrom, self).updateWidgets()
+
+        super(AddForm, self).updateWidgets()
+
+    def nextURL(self):
+        editview = aq_parent(aq_inner(self.context))
+        context = aq_parent(aq_inner(editview))
+        url = str(getMultiAdapter((context, self.request),
+                                  name=u"absolute_url"))
+        return url + '/@@manage-portlets'
+
+    @button.buttonAndHandler(_(u"label_save", default=u"Save"), name='add')
+    def handleAdd(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        obj = self.createAndAdd(data)
+        if obj is not None:
+            # mark only as finished if we get the new object
+            self._finishedAdd = True
+
+    @button.buttonAndHandler(_(u"label_cancel", default=u"Cancel"),
+                             name='cancel_add')
+    def handleCancel(self, action):
+        nextURL = self.nextURL()
+        if nextURL:
+            self.request.response.redirect(nextURL)
+        return ''
+
+    def add(self, obj):
+        ob = self.context.add(obj)
+        self._finishedAdd = True
+        return ob
 
     def create(self, data):
 
-        portal_path_l = len(self.context.portal_url.getPortalObject().getPhysicalPath())
-        assignment_context_path = '/'.join(self.context.__parent__.getPhysicalPath()[portal_path_l:])
+        portal_path_l = len(
+            self.context.portal_url.getPortalObject().getPhysicalPath())
+        assignment_context_path = '/'.join(
+            self.context.__parent__.getPhysicalPath()[portal_path_l:])
 
-        return Assignment(assignment_context_path=assignment_context_path, **data)
+        return Assignment(
+            assignment_context_path=assignment_context_path, **data)
+
 
 class EditForm(form.EditForm):
     form_fields = field.Fields(ITeaserPortlet)
-
 
     label = _(u"Edit Teaser Portlet")
     description = _(u"Shows the given infos on the front-page.")
 
     def updateWidgets(self):
-        self.fields['teaserdesc'].widgetFactory = WYSIWYGWidget
+        self.fields['teaserdesc'].widgetFactory[INPUT_MODE] = WysiwygFieldWidget
         self.fields['internal_target'].widgetFactory = ContentTreeFieldWidget
-        super(EditFrom, self).updateWidgets()
+        super(EditForm, self).updateWidgets()
