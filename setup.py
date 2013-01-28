a5ab2b694f084654daa72cb1e832a4a907929b2e
Jonas Baumann <jone@jone.ch>
2013-01-28 11:21:13 +0100

    Update classifiers.

--- setup.py
+++ setup.py
@@ -15,9 +15,14 @@ setup(name='ftw.subsite',
           open(os.path.join('docs', 'HISTORY.txt')).read(),
 
       # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+
       classifiers=[
-        "Programming Language :: Python",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
 
       keywords='ftw subsite',
