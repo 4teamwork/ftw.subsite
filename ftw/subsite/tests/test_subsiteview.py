29d77de4f584c63bdf67c5929ee8920315e152f2
Timon Tschanz <timon.tschanz@4teamwork.ch>
2013-01-31 16:57:09 +0100

    fix tests, hide original selector

--- ftw/subsite/tests/test_subsiteview.py
+++ ftw/subsite/tests/test_subsiteview.py
@@ -1,14 +1,9 @@
 from ftw.subsite.testing import FTW_SUBSITE_FUNCTIONAL_TESTING
-from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
 from plone.app.testing import TEST_USER_PASSWORD
 from plone.testing.z2 import Browser
-from zope.component import queryMultiAdapter
 import unittest2 as unittest
 import transaction
-from plone.app.testing import logout
-from plone.app.testing import setRoles
-from plone.app.testing import login
 
 
 class TestSubsite(unittest.TestCase):
@@ -41,18 +36,6 @@ class TestSubsite(unittest.TestCase):
     def tearDown(self):
         self.portal.manage_delObjects(['mysubsite'])
         transaction.commit()
-
-    def test_view_has_permission(self):
-        view = queryMultiAdapter((self.subsite, self.portal.REQUEST), name="subsite_view")
-        logout()
-        self.assertFalse(view.hasPermissions())
-        setRoles(self.portal, TEST_USER_ID, ['Member'])
-        login(self.portal, TEST_USER_NAME)
-        self.assertFalse(view.hasPermissions())
-        logout()
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        login(self.portal, TEST_USER_NAME)
-        self.assertTrue(view.hasPermissions())
 
     def test_view_render(self):
         self.browser.open(self.subsite.absolute_url())
