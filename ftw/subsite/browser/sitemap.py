4ff7ea371e7a3d59f13a63de8842304f32dd99f3
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-06-03 19:14:42 +0200

    Make sitemap work relative to a subsite and plone root

---
+++ ftw/subsite/browser/sitemap.py
@@ -0,0 +1,30 @@
+from Acquisition import aq_inner
+from ftw.subsite.utils import get_nav_root
+from plone.app.layout.navigation.interfaces import INavtreeStrategy
+from plone.app.layout.navigation.navtree import buildFolderTree
+from Products.CMFPlone.browser import navigation
+from Products.CMFPlone.browser import navtree
+from zope.component import getMultiAdapter
+
+
+class SubsiteSitemapQueryBuilder(navtree.NavtreeQueryBuilder):
+
+    def __init__(self, context):
+        super(SubsiteSitemapQueryBuilder, self).__init__(context)
+
+        root = get_nav_root(context)
+        self.query['path']['query'] = '/'.join(root.getPhysicalPath())
+
+
+class SubsiteCatalogSiteMap(navigation.CatalogSiteMap):
+
+    def siteMap(self):
+        context = aq_inner(self.context)
+
+        queryBuilder = SubsiteSitemapQueryBuilder(context)
+        query = queryBuilder()
+
+        strategy = getMultiAdapter((context, self), INavtreeStrategy)
+
+        return buildFolderTree(context, obj=context,
+                               query=query, strategy=strategy)
---
+++ ftw/subsite/tests/test_sitemap.py
@@ -0,0 +1,68 @@
+from ftw.subsite.browser.sitemap import SubsiteSitemapQueryBuilder
+from ftw.subsite.testing import FTW_SUBSITE_FUNCTIONAL_TESTING
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_PASSWORD
+from plone.testing.z2 import Browser
+from pyquery import PyQuery
+import transaction
+import unittest2
+
+
+class TestSubsiteSitemap(unittest2.TestCase):
+
+    layer = FTW_SUBSITE_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        super(TestSubsiteSitemap, self).setUp()
+        self.portal = self.layer['portal']
+
+        self.browser = Browser(self.layer['app'])
+        self.browser.handleErrors = False
+        self.browser.addHeader('Authorization', 'Basic %s:%s' % (
+            TEST_USER_NAME, TEST_USER_PASSWORD,))
+
+        self.subsite = self.portal.get(self.portal.invokeFactory(
+            'Subsite',
+            'mysubsite',
+            title="My Subsite"))
+
+        self.folder = self.subsite.get(
+            self.subsite.invokeFactory('Folder', 'folder',
+                                       title="Folder"))
+
+        transaction.commit()
+
+    def test_subsite_query_builder_on_plone_root(self):
+        query = SubsiteSitemapQueryBuilder(self.portal)()
+
+        self.assertEquals(query['path']['query'], '/plone',
+            'Expect portal root')
+
+    def test_subsite_query_builder_on_subsite(self):
+        query = SubsiteSitemapQueryBuilder(self.subsite)()
+
+        self.assertEquals(query['path']['query'], '/plone/mysubsite',
+            'Expect subsite path')
+
+    def test_subsite_query_builder_on_folder_in_subsite(self):
+        query = SubsiteSitemapQueryBuilder(self.folder)()
+
+        self.assertEquals(query['path']['query'], '/plone/mysubsite',
+            'Expect subsite path, because the folder is in a subsite')
+
+    def test_subsite_sitemap_view_on_plone_root(self):
+        self.browser.open("%s/sitemap" % self.portal.absolute_url())
+        doc = PyQuery(self.browser.contents)
+        subsite = doc('#content-core .contenttype-subsite span')
+
+        self.assertEquals(len(subsite), 1, 'Expect to find our Subsite')
+
+    def test_subsite_sitemap_view_on_subsite_root(self):
+        self.browser.open("%s/sitemap" % self.subsite.absolute_url())
+        doc = PyQuery(self.browser.contents)
+        subsite = doc('#content-core .contenttype-subsite span')
+
+        self.assertEquals(len(subsite), 0, 'There should be no subsite')
+
+        folder = doc('#content-core .contenttype-folder span')
+        self.assertEquals(len(folder), 1, 'Expect one folder')
