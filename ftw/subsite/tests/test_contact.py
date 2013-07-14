ef3be9c10b5d3191ac29fbb26d986d1ffb5ce8a4
Jonas Baumann <jone@jone.ch>
2013-07-14 15:04:58 +0200

    Fix email tests.

--- ftw/subsite/tests/test_contact.py
+++ ftw/subsite/tests/test_contact.py
@@ -1,7 +1,8 @@
-import unittest2 as unittest
 from ftw.subsite.testing import FTW_SUBSITE_FUNCTIONAL_TESTING
 from plone.testing.z2 import Browser
+import email
 import transaction
+import unittest2 as unittest
 
 
 class TestContactFrom(unittest.TestCase):
@@ -35,8 +36,8 @@ class TestContactFrom(unittest.TestCase):
         self.browser.getControl(name="form.buttons.53656e64204d61696c").click()
         mh = self.portal.MailHost
         self.assertEqual(len(mh.messages), 1)
-        msg = mh.messages[0]
-        self.assertEqual(msg.get('From'), 'Subsite <blubber@blubb.ch>')
+        msg = email.message_from_string(mh.messages[0])
+        self.assertEqual(msg.get('From'), 'blubber@blubb.ch')
         self.assertEqual(msg.get('reply-to'), u'hans peter <test@test.com>')
         sub = msg.get('Subject')
         self.assertEqual(sub.encode('utf8'), 'Testsubject')
@@ -62,8 +63,8 @@ from your site Peter(http://=\nnohost/plone/mysubsite):\nLorem ipsum dolor sit a
         self.browser.getControl(name="form.buttons.53656e64204d61696c").click()
         mh = self.portal.MailHost
         self.assertEqual(len(mh.messages), 1)
-        msg = mh.messages[0]
-        self.assertEqual(msg.get('From'), 'Ploneroot <site@nohost.com>')
+        msg = email.message_from_string(mh.messages[0])
+        self.assertEqual(msg.get('From'), 'site@nohost.com')
         self.assertEqual(msg.get('reply-to'), u'hans peter <test@test.com>')
         sub = msg.get('Subject')
         self.assertEqual(sub.encode('utf8'), 'Testsubject')
