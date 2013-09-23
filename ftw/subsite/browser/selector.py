a549eec3df55e231c62aee5e664dd3460590f14d
Julian Infanger <j.infanger@4teamwork.ch>
2013-09-23 10:06:17 +0200

    Added browserview returning available languages.

--- ftw/subsite/browser/selector.py
+++ ftw/subsite/browser/selector.py
@@ -3,6 +3,14 @@ from plone.app.layout.viewlets import common
 from zope.component import getMultiAdapter
 from zope.interface import implements
 from zope.viewlet.interfaces import IViewlet
+from zope.publisher.browser import BrowserView
+
+
+class Languages(BrowserView):
+
+    def available(self):
+        langs = getMultiAdapter((self.context, self.request), ILanguages)
+        return bool(langs.get_related_languages())
 
 
 class LanguageSelector(common.ViewletBase):
