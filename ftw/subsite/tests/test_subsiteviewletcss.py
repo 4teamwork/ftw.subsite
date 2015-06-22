05bcc1dd727377444a5e4c425a31699024f49423
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2015-06-22 13:55:36 +0200

    Fix css viewlet tests.

--- ftw/subsite/tests/test_subsiteviewletcss.py
+++ ftw/subsite/tests/test_subsiteviewletcss.py
@@ -1,80 +1,39 @@
+from ftw.builder import Builder
+from ftw.builder import create
 from ftw.subsite.testing import FTW_SUBSITE_FUNCTIONAL_TESTING
-from plone.app.testing import TEST_USER_NAME
-from plone.app.testing import TEST_USER_PASSWORD
-from plone.testing.z2 import Browser
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
-from plone.app.testing import login
-
-from zope.component import queryMultiAdapter
-from zope.viewlet.interfaces import IViewletManager
-from ftw.subsite.browser.subsiteview import SubsiteView
-import unittest2 as unittest
-import transaction
+from ftw.testbrowser import browsing
+from unittest2 import TestCase
 
 
-class TestSubsite(unittest.TestCase):
+class TestSubsite(TestCase):
 
     layer = FTW_SUBSITE_FUNCTIONAL_TESTING
 
     def setUp(self):
         self.portal = self.layer['portal']
-        setRoles(self.portal, TEST_USER_ID, ['Manager', 'Reviewer', 'Contributor'])
-        login(self.portal, TEST_USER_NAME)
 
-        self.browser = Browser(self.layer['app'])
-        self.browser.handleErrors = False
+        self.subsite = create(Builder('subsite')
+                              .titled(u'Subsute')
+                              .having(additional_css='p {color: red;}'))
 
-        self.subsite = self._create_subsite()
-        self.subsite.setAdditional_css('.column > div {color: red;}')
-        transaction.commit()
+        self.styles = '<style type="text/css" media="all">p {color: red;}</style>'
 
-    def _create_subsite(self):
-        subsite = self.portal.get(self.portal.invokeFactory(
-            'Subsite',
-            'mysubsite',
-            title="Peter"))
-        transaction.commit()
+    @browsing
+    def test_cssviewlet_in_subsite(self, browser):
+        browser.login().visit(self.subsite)
 
-        return subsite
+        self.assertIn(self.styles,
+                      browser.contents,
+                      'Did not found the substes styles.')
+        browser.visit(self.portal)
+        self.assertNotIn(self.styles,
+                         browser.contents,
+                         'Found unexpeted subsite styles.')
 
-    def _get_viewlet(self):
-        view = SubsiteView(self.subsite, self.subsite.REQUEST)
-        manager_name = 'plone.portaltop'
-        manager = queryMultiAdapter(
-            (self.subsite, self.subsite.REQUEST, view),
-            IViewletManager,
-            manager_name)
-        self.failUnless(manager)
-        # Set up viewlets
-        manager.update()
-        name = 'ftw.subsite.css'
-        return [v for v in manager.viewlets if v.__name__ == name]
-
-    def _auth(self):
-        self.browser.addHeader('Authorization', 'Basic %s:%s' % (
-            TEST_USER_NAME, TEST_USER_PASSWORD,))
-
-    def tearDown(self):
-        self.portal.manage_delObjects(['mysubsite'])
-        transaction.commit()
-
-    def test_cssviewlet_in_subsite(self):
-        self._auth()
-        self.browser.open(self.subsite.absolute_url())
-        self.assertIn('<style type="text/css" media="all">.column > div {color: red;}</style>', self.browser.contents)
-        self.browser.open(self.portal.absolute_url())
-        self.assertTrue('<style type="text/css" media="all">.column > div {color: red;}</style>' not in self.browser.contents)
-
-    def test_component_registered(self):
-        self.assertTrue(len(self._get_viewlet()) == 1)
-
-    def test_cssviewlet_in_subfolder(self):
-        folder = self.subsite.get(self.subsite.invokeFactory(
-                'Folder',
-                'myfolder',
-                title="Hans"))
-        transaction.commit()
-        self._auth()
-        self.browser.open(folder.absolute_url())
-        self.assertIn('<style type="text/css" media="all">.column > div {color: red;}</style>', self.browser.contents)
+    @browsing
+    def test_cssviewlet_in_subfolder(self, browser):
+        folder = create(Builder('folder').within(self.subsite))
+        browser.login().visit(folder)
+        self.assertIn(self.styles,
+                      browser.contents,
+                      'Did not found the substes styles.')
