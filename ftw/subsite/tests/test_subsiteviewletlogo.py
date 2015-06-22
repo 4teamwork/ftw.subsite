8dc7fb67591aabfa9eb35aa2af6d874f8db3576f
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2015-06-22 12:27:10 +0200

    Fix subsite logo viewlet.

--- ftw/subsite/tests/test_subsiteviewletlogo.py
+++ ftw/subsite/tests/test_subsiteviewletlogo.py
@@ -1,48 +1,34 @@
+from ftw.builder import Builder
+from ftw.builder import create
 from ftw.subsite.browser.subsiteview import SubsiteView
 from ftw.subsite.interfaces import IFtwSubsiteLayer
 from ftw.subsite.testing import FTW_SUBSITE_FUNCTIONAL_TESTING
-from plone.app.testing import TEST_USER_NAME
+from ftw.testbrowser import browsing
+from plone.namedfile.file import NamedBlobImage
 from plone.app.testing import TEST_USER_ID
-from plone.app.testing import TEST_USER_PASSWORD
-from plone.testing.z2 import Browser
+from unittest2 import TestCase
 from zope.component import queryMultiAdapter
-from zope.viewlet.interfaces import IViewletManager
 from zope.interface import alsoProvides
+from zope.viewlet.interfaces import IViewletManager
 import os
 import transaction
-import unittest2 as unittest
 
 
-class TestLogoViewlet(unittest.TestCase):
+class TestLogoViewlet(TestCase):
 
     layer = FTW_SUBSITE_FUNCTIONAL_TESTING
 
     def setUp(self):
         self.portal = self.layer['portal']
 
-        self.browser = Browser(self.layer['app'])
-        self.browser.handleErrors = False
-
-    def _auth(self):
-        self.browser.addHeader('Authorization', 'Basic %s:%s' % (
-            TEST_USER_NAME, TEST_USER_PASSWORD,))
-
-    def _create_subsite(self):
-        subsite = self.portal.get(self.portal.invokeFactory(
-            'Subsite',
-            'mysubsite',
-            title="MySubsite"))
-
-        transaction.commit()
-        return subsite
+        self.subsite = create(Builder('subsite').titled(u'MySubsite'))
 
     def _get_viewlet(self):
-        subsite = self._create_subsite()
-        alsoProvides(subsite.REQUEST, IFtwSubsiteLayer)
-        view = SubsiteView(subsite, subsite.REQUEST)
+        alsoProvides(self.subsite.REQUEST, IFtwSubsiteLayer)
+        view = SubsiteView(self.subsite, self.subsite.REQUEST)
         manager_name = 'plone.portalheader'
         manager = queryMultiAdapter(
-            (subsite, subsite.REQUEST, view),
+            (self.subsite, self.subsite.REQUEST, view),
             IViewletManager,
             manager_name)
         self.failUnless(manager)
@@ -51,90 +37,73 @@ class TestLogoViewlet(unittest.TestCase):
         name = 'subsite.logo'
         return [v for v in manager.viewlets if v.__name__ == name]
 
-    def tearDown(self):
-        if 'mysubsite' in self.portal.objectIds():
-            self.portal.manage_delObjects(['mysubsite'])
-            transaction.commit()
+    def assert_subsite_logo_src(self, browser):
+        img_src = browser.css('#portal-logo img').first.attrib['src']
+        expected_part_image_src = '{0}/@@images/'.format(
+            self.subsite.absolute_url())
+        assert (img_src.startswith(expected_part_image_src),
+                'Wrong logo path, expect img src starting with: {0}'.format(
+            expected_part_image_src))
 
     def test_component_registered(self):
         self.assertTrue(len(self._get_viewlet()) == 1)
 
-    def test_without_logo(self):
-        subsite = self._create_subsite()
-        self._auth()
-        self.browser.open(subsite.absolute_url())
-        self.assertNotIn("%s/@@images" % subsite.absolute_url(),
-                         self.browser.contents)
+    @browsing
+    def test_without_logo(self, browser):
+        browser.login().visit(self.subsite)
 
-        self.assertNotIn('alt="MySubsite"', self.browser.contents)
-        self.browser.open(self.portal.absolute_url())
-        # No logo.png on subsite
-        self.assertNotIn("%s/logo.png" % subsite.absolute_url(),
-                         self.browser.contents)
+        self.assertEquals(self.subsite.absolute_url(),
+                          browser.css('#portal-logo').first.attrib['href'])
 
-        # There should be no change on plone root
-        self.assertIn("%s/logo.png" % self.portal.absolute_url(),
-                      self.browser.contents)
+        self.assertEquals('http://nohost/plone/logo.png',
+                          browser.css('#portal-logo img').first.attrib['src'])
 
-    def test_with_logo(self):
-        subsite = self._create_subsite()
+        browser.visit(self.portal)
+        self.assertEquals('http://nohost/plone/logo.png',
+                          browser.css('#portal-logo img').first.attrib['src'])
+
+    @browsing
+    def test_with_logo(self, browser):
         file_ = open("%s/blue.png" % os.path.split(__file__)[0], 'r')
         file_.seek(0)
-        subsite.setLogo(file_)
+        self.subsite.logo = NamedBlobImage(data=file_.read(),
+                                           filename=u'logo.png')
         transaction.commit()
 
-        self._auth()
-        self.browser.open(subsite.absolute_url())
-        self.assertIn("%s/@@images" % subsite.absolute_url(),
-                      self.browser.contents)
+        browser.login().visit(self.subsite)
 
-        self.assertIn('alt="MySubsite"', self.browser.contents)
-        self.browser.open(self.portal.absolute_url())
-        # No logo.png on subsite
-        self.assertNotIn("%s/logo.png" % subsite.absolute_url(),
-                         self.browser.contents)
+        self.assertEquals(self.subsite.absolute_url(),
+                          browser.css('#portal-logo').first.attrib['href'])
 
-        # There should be no change on plone root
-        self.assertIn("%s/logo.png" % self.portal.absolute_url(),
-                      self.browser.contents)
+        self.assert_subsite_logo_src(browser)
 
-    def test_with_logo_subcontent(self):
-        subsite = self._create_subsite()
+        browser.visit(self.portal)
+        self.assertEquals('http://nohost/plone/logo.png',
+                          browser.css('#portal-logo img').first.attrib['src'])
+
+    @browsing
+    def test_with_logo_subcontent(self, browser):
         file_ = open("%s/blue.png" % os.path.split(__file__)[0], 'r')
-        subsite.setLogo(file_)
-
-        # Create more content
-        folder = subsite.get(subsite.invokeFactory('Folder', 'folder'))
-        folder1 = folder.get(folder.invokeFactory('Folder', 'folder'))
-
+        file_.seek(0)
+        self.subsite.logo = NamedBlobImage(data=file_.read(),
+                                           filename=u'logo.png')
         transaction.commit()
 
-        self._auth()
-        self.browser.open(folder1.absolute_url())
-        self.assertIn("%s/@@images" % subsite.absolute_url(),
-                      self.browser.contents)
+        subfolder = create(Builder('folder').within(self.subsite))
+        subsubfolder = create(Builder('folder').within(subfolder))
 
-        self.assertIn('alt="MySubsite"', self.browser.contents)
-        self.browser.open(self.portal.absolute_url())
+        browser.login().visit(subsubfolder)
+        self.assert_subsite_logo_src(browser)
 
-        # There should be no change on plone root
-        self.assertIn("%s/logo.png" % self.portal.absolute_url(),
-                      self.browser.contents)
-
-    def test_plone_logo_in_factory(self):
-        self._auth()
-        self.browser.open(
-            self.portal.absolute_url() + '/createObject?type_name=Subsite')
-
-        self.assertNotIn("/@@images/", self.browser.contents)
-
-        self.assertIn("%s/logo.png" % self.portal.absolute_url(),
-                      self.browser.contents)
-
-    def test_logo_in_portal_tools(self):
-        subsite = self._create_subsite()
+    @browsing
+    def test_logo_in_portal_tools(self, browser):
         file_ = open("%s/blue.png" % os.path.split(__file__)[0], 'r')
-        subsite.setLogo(file_)
+        file_.seek(0)
+        self.subsite.logo = NamedBlobImage(data=file_.read(),
+                                           filename=u'logo.png')
         transaction.commit()
-        self._auth()
-        self.browser.open(subsite.absolute_url() + '/mail_password?userid=' + TEST_USER_ID)
+        browser.login().visit(self.subsite,
+                              view='mail_password',
+                              data={'userid': TEST_USER_ID})
+
+        self.assert_subsite_logo_src(browser)
