c14dac3be57e857bc063a0d564052bdd409b0bbb
Nick Bernd Espig <info@nachtalb.io>
2019-12-19 13:54:42 +0100

    Simplyfie JS for subsite use

--- ftw/subsite/browser/resources/dropdown.js
+++ ftw/subsite/browser/resources/dropdown.js
@@ -7,52 +7,30 @@
         factory(root.jQuery);
     }
 }(typeof self !== 'undefined' ? self : this, function ($) {
-  function hideAllMenus() {
-      $('dl.actionMenu').removeClass('activated').addClass('deactivated');
+  function hide() {
+      $('#portal-languageselector').removeClass('activated').addClass('deactivated');
   }
 
-  function toggleMenuHandler(event) {
-      // swap between activated and deactivated
-      $(this).parents('.actionMenu:first')
+  function toggle() {
+      $('#portal-languageselector')
           .toggleClass('deactivated')
           .toggleClass('activated');
       return false;
   }
 
-  function actionMenuDocumentMouseDown(event) {
-      if ($(event.target).parents('.actionMenu:first').length) {
-          // target is part of the menu, so just return and do the default
-          return true;
-      }
-
-      hideAllMenus();
-  }
-
-  function actionMenuMouseOver(event) {
-      var menu_id = $(this).parents('.actionMenu:first').attr('id'),
-          switch_menu;
-      if (!menu_id) {return true;}
-
-      switch_menu = $('dl.actionMenu.activated').length > 0;
-      $('dl.actionMenu').removeClass('activated').addClass('deactivated');
-      if (switch_menu) {
-          $('#' + menu_id).removeClass('deactivated').addClass('activated');
+  function hideOnOutsideClick(event) {
+      if ($(event.target).parents('#portal-languageselector').length === 0) {
+          hide();
       }
   }
 
-  function initializeMenus() {
-      $(document).mousedown(actionMenuDocumentMouseDown);
+  function initializeMenu() {
+      hide();
 
-      hideAllMenus();
+      $(document).on('mousedown', hideOnOutsideClick);
 
-      // add toggle function to header links
-      $('dl.actionMenu dt.actionMenuHeader a')
-          .click(toggleMenuHandler)
-          .mouseover(actionMenuMouseOver);
-
-      // add hide function to all links in the dropdown, so the dropdown closes
-      // when any link is clicked
-      $('dl.actionMenu > dd.actionMenuContent').click(hideAllMenus);
+      $('#portal-languageselector .actionMenuHeader a').on('click', toggle)
+      $('#portal-languageselector .actionMenuContent').on('click', hide);
   }
 
   $(initializeMenus);
