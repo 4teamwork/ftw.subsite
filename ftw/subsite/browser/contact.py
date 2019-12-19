864a1da3dc7665cfd5e14a13cff22f66fbcf46ad
Nick Bernd Espig <info@nachtalb.io>
2019-12-19 15:01:09 +0100

    Remove obsolete IS_PLONE_5 expression

--- ftw/subsite/browser/contact.py
+++ ftw/subsite/browser/contact.py
@@ -3,6 +3,7 @@ from Products.statusmessages.interfaces import IStatusMessage
 from email.header import Header
 from email.mime.text import MIMEText
 from ftw.subsite import _
+from ftw.subsite import IS_PLONE_5
 from plone.app.layout.navigation.root import getNavigationRoot
 from plone.dexterity.utils import safe_utf8
 from z3c.form import form, field, button
@@ -13,11 +14,7 @@ from zope.component import provideAdapter
 from zope.i18n import translate
 from zope.interface import Interface
 from zope.interface import Invalid
-import pkg_resources
 import re
-
-
-IS_PLONE_5 = pkg_resources.get_distribution('Products.CMFPlone').version >= '5'
 
 
 class IContactForm(Interface):
