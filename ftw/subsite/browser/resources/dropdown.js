af4f9a14bfe646d8487943af5fb5e79881624714
Nick Bernd Espig <info@nachtalb.io>
2019-12-19 15:03:46 +0100

    Reindent

--- ftw/subsite/browser/resources/dropdown.js
+++ ftw/subsite/browser/resources/dropdown.js
@@ -1,36 +1,36 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module. (Plone 5 with requirejs)
-        define(['jquery'], factory);
-    } else {
-        // Browser globals (Plone 4 without requirejs)
-        factory(root.jQuery);
-    }
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module. (Plone 5 with requirejs)
+    define(['jquery'], factory);
+  } else {
+    // Browser globals (Plone 4 without requirejs)
+    factory(root.jQuery);
+  }
 }(typeof self !== 'undefined' ? self : this, function ($) {
   function hide() {
-      $('#portal-languageselector').removeClass('activated').addClass('deactivated');
+    $('#portal-languageselector').removeClass('activated').addClass('deactivated');
   }
 
   function toggle() {
-      $('#portal-languageselector')
-          .toggleClass('deactivated')
-          .toggleClass('activated');
-      return false;
+    $('#portal-languageselector')
+      .toggleClass('deactivated')
+      .toggleClass('activated');
+    return false;
   }
 
   function hideOnOutsideClick(event) {
-      if ($(event.target).parents('#portal-languageselector').length === 0) {
-          hide();
-      }
+    if ($(event.target).parents('#portal-languageselector').length === 0) {
+      hide();
+    }
   }
 
   function initializeMenu() {
-      hide();
+    hide();
 
-      $(document).on('mousedown', hideOnOutsideClick);
+    $(document).on('mousedown', hideOnOutsideClick);
 
-      $('#portal-languageselector .actionMenuHeader a').on('click', toggle)
-      $('#portal-languageselector .actionMenuContent').on('click', hide);
+    $('#portal-languageselector .actionMenuHeader a').on('click', toggle)
+    $('#portal-languageselector .actionMenuContent').on('click', hide);
   }
 
   $(initializeMenus);
